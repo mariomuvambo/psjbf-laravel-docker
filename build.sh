06b76b0fec016babe8199fe5c0c23c12b1bcd074
Mario Muvambo <mariomuvambo1@gmail.com>
2025-07-23 13:55:10 +0200

    Add and make build.sh executable

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+# Aborta se algum comando falhar
+set -e
+
+# Instala dependências PHP
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+# Instala dependências do Node e compila o frontend com Vite
 echo "➡️ Build do frontend (Vite)"
 cd resources/js
 npm install
 npm run build
 cd ../..
 
+# Gera cache e outras otimizações do Laravel
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 echo "✅ Build frontend concluído"
