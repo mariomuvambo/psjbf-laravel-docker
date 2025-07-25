adab73efaa7b86ed77f72ed46d4802e98a52b932
Mario Muvambo <mariomuvambo1@gmail.com>
2025-07-25 21:00:45 +0200

    Ajustes novos para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
@@ -11,13 +11,11 @@ export default defineConfig({
         vue(),
     ],
     build: {
-        outDir: 'public/build',  // Laravel espera os arquivos aqui
+        outDir: 'public/build', // Laravel espera que os arquivos de build fiquem aqui
     },
     server: {
-        host: '0.0.0.0',  // Permite conexões externas, importante para Render
-        port: 80,         // Porta de comunicação no Render
-    },
-    preview: {
-        allowedHosts: ['psjbf.onrender.com'],  // Adiciona o domínio aqui para permitir acesso
+        host: '0.0.0.0',  // Força o servidor a escutar em todas as interfaces
+        port: 80,          // Defina a porta como 80
+        strictPort: true,  // Garante que a porta 80 seja usada
     },
 });
