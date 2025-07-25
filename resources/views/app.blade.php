71ad1b8440e702969702ca4b20b24bc9be6188be
Mario Muvambo <mariomuvambo1@gmail.com>
2025-07-25 21:29:42 +0200

    Ajustes resfactore

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel + Vue</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+
 </head>
 <body>
     <div id="app"></div>
