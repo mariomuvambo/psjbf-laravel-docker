18b5d59a2ccfe74d7886ec5b9aa7551ed33d12e4
Mario Muvambo <mariomuvambo1@gmail.com>
2025-07-26 19:17:32 +0200

    Preparando para deploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel + Vue</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
-
+    @vite(['resources/js/app.js'])
 </head>
 <body>
     <div id="app"></div>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -9,18 +9,8 @@ export default defineConfig({
             refresh: true,
         }),
         vue(),
+        
     ],
-    build: {
-        outDir: 'public/build', // Laravel espera que os arquivos de build fiquem aqui
-    },
-   server: {
-    host: '0.0.0.0',  // Pode ser 'localhost' ou '127.0.0.1' em desenvolvimento
-    port: 80, 
-    strictPort: true,  
-    open: true,  // Para abrir a página automaticamente
-},
-preview: {
-    allowedHosts: ['psjbf.onrender.com'],  // Domínio correto para produção
-},
-
 });
+
+
