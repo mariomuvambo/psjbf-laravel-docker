538add42d7bbecaa72ea2ffdfe60a0a68a84dd59
Mario Muvambo <mariomuvambo1@gmail.com>
2025-10-20 04:37:46 +0200

    fix: ensure pdo_pgsql

--- config/database.php
+++ config/database.php
@@ -35,13 +35,24 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        'connections' => [
+            'pgsql' => [
+                'driver' => 'pgsql',
+                'host' => env('DB_HOST', '127.0.0.1'),
+                'port' => env('DB_PORT', '5432'),
+                'database' => env('DB_DATABASE', 'forge'),
+                'username' => env('DB_USERNAME', 'forge'),
+                'password' => env('DB_PASSWORD', ''),
+                'charset' => 'utf8',
+                'prefix' => '',
+                'schema' => 'public',
+                'sslmode' => 'prefer',
+            ],
         ],
+        'default' => env('DB_CONNECTION', 'pgsql'),
+
+
+        
 
         'mysql' => [
             'driver' => 'mysql',
