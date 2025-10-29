075f0db3f0d5bf832aa2b223d5a5f449bdc43997
Mario Muvambo <mariomuvambo1@gmail.com>
2025-10-29 11:45:10 +0200

    perfilUser

--- app/Http/Controllers/ProfiluserController.php
+++ app/Http/Controllers/ProfiluserController.php
@@ -8,13 +8,36 @@ use Illuminate\Support\Facades\Storage;
 
 class ProfiluserController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
+        //
         return response()->json(Profiluser::all(), 200);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
+        //
         $request->validate([
             'name' => 'required|string',
             'surname' => 'required|string',
@@ -23,11 +46,11 @@ class ProfiluserController extends Controller
             'minister' => 'required|string',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
-
-        $imagePath = null;
+        
 
         if ($request->hasFile('image')) {
-            $imagePath = $request->file('image')->store('profilusers', 'public');
+            $imagePath = $request->file('image')->store('profilusers', 'public'); 
+
         }
 
         $profiluser = Profiluser::create([
@@ -36,21 +59,44 @@ class ProfiluserController extends Controller
             'date_birth' => $request->date_birth,
             'nucleo' => $request->nucleo,
             'minister' => $request->minister,
-            'image' => $imagePath,
+            'image' => asset('storage/' . $imagePath),
+
         ]);
 
         return response()->json($profiluser, 201);
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show(Profiluser $profiluser)
     {
-        $profiluser->image = $profiluser->image
-            ? asset('storage/' . $profiluser->image)
-            : null;
-
+        $profiluser->image = $profiluser->image ? asset('storage/' . $profiluser->image) : null;
         return response()->json($profiluser, 200);
     }
+    
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, Profiluser $profiluser)
     {
         $validated = $request->validate([
@@ -63,13 +109,12 @@ class ProfiluserController extends Controller
         ]);
 
         if ($request->hasFile('image')) {
-            // apaga imagem anterior se existir
-            if ($profiluser->image && Storage::disk('public')->exists($profiluser->image)) {
-                Storage::disk('public')->delete($profiluser->image);
+            if ($profiluser->image && Storage::disk('public')->exists(str_replace('/storage/', '', $profiluser->image))) {
+                Storage::disk('public')->delete(str_replace('/storage/', '', $profiluser->image));
             }
 
             $imagePath = $request->file('image')->store('profilusers', 'public');
-            $validated['image'] = $imagePath;
+            $validated['image'] = '/storage/' . $imagePath;
         }
 
         $profiluser->update($validated);
@@ -77,10 +122,20 @@ class ProfiluserController extends Controller
         return response()->json($profiluser, 200);
     }
 
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy(Profiluser $profiluser)
     {
-        if ($profiluser->image && Storage::disk('public')->exists($profiluser->image)) {
-            Storage::disk('public')->delete($profiluser->image);
+        if ($profiluser->image) {
+            $imagePath = str_replace('/storage/', '', $profiluser->image);
+            if (Storage::disk('public')->exists($imagePath)) {
+                Storage::disk('public')->delete($imagePath);
+            }
         }
 
         $profiluser->delete();
@@ -88,11 +143,13 @@ class ProfiluserController extends Controller
         return response()->json(['message' => 'Perfil excluído com sucesso!'], 200);
     }
 
+
     public function processo()
     {
         $user = auth()->user();
-        $processo = $user->processos()->latest()->first();
-
+        $processo = $user->processos()->latest()->first(); 
         return response()->json($processo);
     }
+
+
 }
